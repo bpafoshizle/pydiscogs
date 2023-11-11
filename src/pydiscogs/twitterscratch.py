fed833f057b746138e6ffa44bd883ed4d3595103
bpafoshizle <bpafoshizle@gmail.com>
2023-11-11 15:24:46 -0600

    linting stuff I don't even care about

--- src/pydiscogs/twitterscratch.py
+++ src/pydiscogs/twitterscratch.py
@@ -8,20 +8,20 @@ twitter = tweepy.Client(
 )
 
 
-## Function to return yesterday's date in YYYY-MM-DDTHH:mm:ssZ format
+# Function to return yesterday's date in YYYY-MM-DDTHH:mm:ssZ format
 def get_yesterday():
     yesterday = datetime.now() - timedelta(days=1)
     return yesterday.strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
-## Function to return timestamp of 1 hour ago in the YYYY-MM-DDTHH:mm:ssZ format
+# Function to return timestamp of 1 hour ago in the YYYY-MM-DDTHH:mm:ssZ format
 def get_hour_ago():
     hour_ago = datetime.now() - timedelta(hours=1)
     return hour_ago.strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
-## Function to get the top tweets from a user over the past 24 hours
-## not including retweets or replies, and the count of likes and retweets
+# Function to get the top tweets from a user over the past 24 hours
+# not including retweets or replies, and the count of likes and retweets
 def get_top_tweets(user, count=10):
     tweets = twitter.search_recent_tweets(
         f"from:{user} -is:retweet -is:reply",
@@ -36,13 +36,13 @@ def get_top_tweets(user, count=10):
     return tweets
 
 
-## Function to print items from a list on separate lines with separators
+# Function to print items from a list on separate lines with separators
 def print_list(list, separator="\n"):
     for item in list:
         print(f"###TWEET###\n{item.public_metrics}\n{item}\n\n", end=separator)
 
 
-## Function to sort a list of tweets by number of likes
+# Function to sort a list of tweets by number of likes
 def sort_by_likes(tweets):
     return sorted(
         tweets, key=lambda tweet: tweet.public_metrics["like_count"], reverse=True
