c73a5c8ae878aaacbcf1cec9655eeb319375c116
bpafoshizle <bpafoshizle@gmail.com>
2024-10-24 22:13:22 -0500

    updated to yfinance mostly, little bit of polygon

--- src/pydiscogs/cogs/stocks.py
+++ src/pydiscogs/cogs/stocks.py
@@ -4,9 +4,11 @@ from typing import List
 
 import aiohttp
 import discord
+import yfinance as yf
 from bs4 import BeautifulSoup
 from discord.ext import commands, tasks
 from polygon import RESTClient
+
 
 # from icecream import ic
 from pydiscogs.utils.timing import calc_tomorrow_7am, wait_until
@@ -19,21 +21,16 @@ class StockQuote(commands.Cog):
         self,
         bot,
         stock_list: List[str],
-        polygon_token,
+        polygon_api_key: str,
         discord_post_channel_id,
     ):
         self.bot = bot
         self.stock_list = stock_list
-        self.polygon_token = polygon_token
         self.discord_post_channel_id = discord_post_channel_id
+        self.polygon_client = RESTClient(api_key=polygon_api_key)
 
         # pylint: disable=no-member
         self.stock_morning_report_task.start()
-
-        # bot.slash_command(guild_ids=guild_ids)(self.stockquote)
-        # bot.slash_command(guild_ids=guild_ids)(self.stockclose)
-        # bot.slash_command(guild_ids=guild_ids)(self.stocknews)
-        # bot.slash_command(guild_ids=guild_ids)(self.marketnews)
 
     @commands.slash_command()
     async def stockquote(self, ctx, symbol):
@@ -50,24 +47,17 @@ class StockQuote(commands.Cog):
         await ctx.respond(embed=stock_close)
 
     @commands.slash_command()
-    async def stockclose2(self, ctx, symbol):
-        stock_close = self.formatPrevCloseEmbed(
-            *await self.getPrevCloseV2(symbol.upper())
-        )
-        await ctx.respond(embed=stock_close)
-
-    @commands.slash_command()
     async def stocknews(self, ctx, symbol):
         stock_news = self.formatStockNewsEmbed(
-            await self.getStockNewsMarketWatch(symbol)
+            await self.getStockNewsyfinance(symbol)
         )
         for article in stock_news:
             logger.debug(article)
             await ctx.respond(embed=article)
-
+    
     @commands.slash_command()
-    async def marketnews(self, ctx):
-        stock_news = self.formatStockNewsEmbed(await self.getLatestMarketWatch())
+    async def getlateststocknews(self, ctx):
+        stock_news = self.formatStockNewsEmbed(await self.getLatestNewsStockList())
         for article in stock_news:
             logger.debug(article)
             await ctx.respond(embed=article)
@@ -77,7 +67,7 @@ class StockQuote(commands.Cog):
         logger.info("channel id %s", self.discord_post_channel_id)
         chnl = self.bot.get_channel(int(self.discord_post_channel_id))
         logger.info("Got channel %s", chnl)
-        stock_news = self.formatStockNewsEmbed(await self.getLatestMarketWatch())
+        stock_news = self.formatStockNewsEmbed(await self.getLatestNewsStockList())
         for article in stock_news:
             logger.debug(article)
             await chnl.send(embed=article)
@@ -93,133 +83,58 @@ class StockQuote(commands.Cog):
         await wait_until(tmrw_7am)
         logger.info("stock_morning_report_task.before_loop: waited until 7am")
 
-    async def getStockNewsPolygon(self, symbol):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                f"https://api.polygon.io/v1/meta/symbols/{symbol}/news?perpage=5&page=1&apiKey={self.polygon_token}"
-            ) as r:
-                if r.status == 200:
-                    news = await r.json()
-                    logger.debug(news)
-                    return news
-
-    async def getLatestMarketWatch(self):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                "https://www.marketwatch.com/latest-news?mod=top_nav"
-            ) as r:
-                if r.status == 200:
-                    return self.parseMarketWatch(await r.text())
-
-    async def getStockNewsMarketWatch(self, symbol):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                f"https://www.marketwatch.com/investing/stock/{symbol}?mod=quote_search"
-            ) as r:
-                if r.status == 200:
-                    return self.parseMarketWatch(await r.text())
-
-    def parseMarketWatch(self, responseText):
-        soup = BeautifulSoup(responseText, features="html.parser")
-        soup = soup.find(
-            "div", attrs={"class": ["collection__elements", "j-scrollElement"]}
-        )
-        soup = soup.find_all("div", attrs={"class": ["element--article"]})[:5]
-        news = []
-        for section in soup:
-            article = {}
-            article["timestamp"] = getattr(
-                section.find("span", class_="article__timestamp"),
-                "string",
-                datetime.now(),
-            )
-            article["source"] = getattr(
-                section.find("span", class_="article__author"), "string", "by Unknown"
-            )
-            if "no-image" not in section["class"]:
-                article["image"] = (
-                    section.find("a", class_="figure__image")
-                    .img["data-srcset"]
-                    .split(",")[2]
-                    .split()[0]
-                )
-                # article["url"] = section.find("a", class_="figure__image")["href"]
+    async def getStockNewsyfinance(self, symbol, maxcount=5):
+        ticker = yf.Ticker(symbol)
+        return ticker.news[:maxcount]
+    
+    async def getLatestNewsStockList(self):
+        stock_news = []
+        for stock in self.stock_list:
             try:
-                article["title"] = section.find(
-                    "h3", class_="article__headline"
-                ).a.string.strip()
-                article["url"] = section.find("h3", class_="article__headline").a[
-                    "href"
-                ]
-            except AttributeError:
-                try:
-                    article["title"] = section.find(
-                        "h3", class_="article__headline"
-                    ).span.string.strip()
-                except AttributeError:
-                    article["title"] = "No title found"
-                    logger.info(
-                        "No article__headline in no-image class element. Section: %s",
-                        section,
-                    )
-
-            news.append(article)
-        return news
-
-    async def getPrevClose(self, symbol):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                f"https://api.polygon.io/v2/aggs/ticker/{symbol}/prev?unadjusted=true&apiKey={self.polygon_token}"
-            ) as r:
-                if r.status == 200:
-                    json_data = await r.json()
-                    # ic(json_data)
-                    logger.debug(json_data)
-                    prev_close = json_data["results"][0]["c"]
-                    prev_high = json_data["results"][0]["h"]
-                    prev_low = json_data["results"][0]["l"]
-                    return (symbol, prev_close, prev_high, prev_low)
+                stock_news.append((await self.getStockNewsyfinance(stock, 1))[0])
+            except IndexError:
+                pass
+        return stock_news   
                 
-    async def getPrevCloseV2(self, symbol):
-        client = RESTClient() # uses POLYGON_API_KEY
-        prevClose = client.get_previous_close(symbol)
+    async def getPrevClose(self, symbol):
+        prevClose = self.polygon_client.get_previous_close_agg(symbol)[0]
         logger.debug(prevClose)
         return (prevClose.ticker, prevClose.close, prevClose.high, prevClose.low)
-        
 
-    
     async def getLatestStockQuote(self, symbol):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                f"https://finance.yahoo.com/quote/{symbol}?p={symbol}"
-            ) as r:
-                if r.status == 200:
-                    logger.debug(
-                        f"received response from https://finance.yahoo.com/quote/{symbol}?p={symbol}"
-                    )
-                    soup = BeautifulSoup(await r.text(), features="html.parser")
-                    soup = soup.find(id="quote-header-info")
-                    name = soup.h1.text
-                    lastprice = soup.find(
-                        "fin-streamer", attrs={"data-field": "regularMarketPrice"}
-                    )["value"]
-                    change = soup.find(
-                        "fin-streamer", attrs={"data-field": "regularMarketChange"}
-                    )["value"]
-                    pctchange = soup.find(
-                        "fin-streamer",
-                        attrs={"data-field": "regularMarketChangePercent"},
-                    )["value"]
-                    quotetime = soup.find("div", id="quote-market-notice").string
+        ticker = yf.Ticker(symbol)
+        # print all attributes and values of fast_info
 
-                    return (symbol, name, lastprice, change, pctchange, quotetime)
+        if ticker.fast_info.previous_close is not None:
+            change = ticker.fast_info.last_price - ticker.fast_info.previous_close
+            pctchange = change / ticker.fast_info.previous_close * 100
+        else:
+            change = 0
+            pctchange = 0
+        
+        # Safely handle missing 'Earnings Date' in the 'calendar' dictionary
+        try:
+            earnings_date = ticker.calendar['Earnings Date'][0]
+        except (KeyError, IndexError):
+            earnings_date = 'N/A'
+
+        return (
+            ticker.info.get('symbol', 'N/A'),
+            ticker.info.get('shortName', 'N/A'),
+            str(round(ticker.fast_info.last_price, 2)) if hasattr(ticker.fast_info, 'last_price') else 'N/A',
+            str(round(change, 2)) if isinstance(change, (int, float)) else 'N/A',
+            str(round(pctchange, 2)) if isinstance(pctchange, (int, float)) else 'N/A',
+            str(datetime.now()),
+            earnings_date
+        )
 
     def formatLatestStockQuoteEmbed(
-        self, symbol, name, lastprice, change, pctchange, quotetime
+        self, symbol, name, lastprice, change, pctchange, quotetime,
+        earningsdate
     ):
         embed = discord.Embed(
             title="Stock Latest Quote",
-            description="Most recent quote from your friendly Egroup bot",
+            description="Most recent quote from your stock bot",
             color=0x9D2235,
         )
         embed.add_field(name="Symbol", value=symbol)
@@ -228,6 +143,7 @@ class StockQuote(commands.Cog):
         embed.add_field(name="Last Price", value=lastprice)
         embed.add_field(name="Change", value=change)
         embed.add_field(name="% Change", value=pctchange)
+        embed.add_field(name="Earnings Date", value=earningsdate)
         return embed
 
     def formatPrevCloseEmbed(self, symbol, prev_close, prev_high, prev_low):
@@ -246,12 +162,19 @@ class StockQuote(commands.Cog):
         embeds = []
         for article in news:
             embed = discord.Embed(
-                title=article["title"], url=article.get("url", ""), color=0x9D2235
+                title=article["title"], url=article.get("link", ""), color=0x9D2235
             )
-            embed.set_image(url=article.get("image", ""))
-            embed.add_field(name="Source", value=article["source"])
+            try:
+                thumbnail = article.get("thumbnail", {})
+                resolutions = thumbnail.get("resolutions", [{}])
+                url = resolutions and resolutions[0].get("url", "")
+                embed.set_image(url=url)
+            except (KeyError, IndexError, AttributeError) as e:
+                logger.debug("Error setting image: %s", e)
+                embed.set_image(url="")
+            embed.add_field(name="Source", value=article["publisher"])
             embed.add_field(
-                name="Timestamp", value=article.get("timestamp", datetime.now())
+                name="Timestamp", value=datetime.fromtimestamp(article.get("providerPublishTime", datetime.now().timestamp())).strftime("%Y-%m-%d %H:%M:%S")
             )
             embeds.append(embed)
         return embeds
