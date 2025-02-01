0c6db64cfc0c08a43fe37b41d0d1a060920e1e61
bpafoshizle <bpafoshizle@gmail.com>
2025-02-01 16:55:19 -0600

    basic replies and history now working. have to use right-click->apps->AI Reply, via 'Message Command' though so UX isn't great. May need to look into bridge_commands that allow using slash commands and prefix commands that share the same code base

--- src/pydiscogs/cogs/ai.py
+++ src/pydiscogs/cogs/ai.py
@@ -19,6 +19,42 @@ class AI(commands.Cog):
         groq_llm_model: str = os.getenv("GROQ_LLM_MODEL"),
         ai_system_prompt: str = os.getenv("AI_SYSTEM_PROMPT")
     ):
+        self.ai_handler = AIHandler(
+            ollama_endpoint, groq_api_key, groq_llm_model, ai_system_prompt
+        )
+        self.bot = bot
+    
+    @commands.slash_command()
+    async def ai(self, ctx: discord.ApplicationContext, input: str):
+        await ctx.defer()
+        response = await self.ai_handler.call(input)
+        await ctx.respond(response)
+
+    @commands.message_command(name="AI Reply")
+    async def ai_reply(self, ctx, message: discord.Message):
+        modal = AIReplyModal(title="AI Reply", ai_handler=self.ai_handler, message_content=message.content)
+        await ctx.send_modal(modal)
+    
+
+class AIReplyModal(discord.ui.Modal):
+    def __init__(self, *args, ai_handler, message_content, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.ai_handler = ai_handler
+        self.message_content = message_content
+        self.add_item(discord.ui.InputText(label="Prompt", style=discord.InputTextStyle.long))
+
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        response = await self.ai_handler.call(self.children[0].value, self.message_content)
+        await interaction.followup.send(response)
+
+class AIHandler():
+    def __init__(self,
+        ollama_endpoint: str = os.getenv("OLLAMA_ENDPOINT"),
+        groq_api_key: str = os.getenv("GROQ_API_KEY"),
+        groq_llm_model: str = os.getenv("GROQ_LLM_MODEL"),
+        ai_system_prompt: str = os.getenv("AI_SYSTEM_PROMPT")
+    ):
         if ollama_endpoint is None and groq_api_key is None:
             raise ValueError(
                 "Must specify either ollama_endpoint or groq_api_key"
@@ -30,6 +66,7 @@ class AI(commands.Cog):
                     "system",
                     ai_system_prompt,
                 ),
+                ("human", "replied to message: {replied_to_message_content}"),
                 ("human", "{input}"),
             ]
         )
@@ -40,34 +77,28 @@ class AI(commands.Cog):
         if ollama_endpoint and groq_api_key:
             self.current_llm = self.ollama_llm
             self.fallback_llm = self.groq_llm
-        # else:
-        #     self.ollama_llm = ChatOllama(
-        #         model="llama3.2",
-        #         num_predict=330,
-        #         temperature=0,
-        #     )
-        #     self.llm_chain = self.prompt | self.ollama_llm
 
         self.llm_chain = self.prompt | self.current_llm
-        self.bot = bot
 
-    @commands.slash_command()
-    async def ai(self, ctx: discord.ApplicationContext, prompt: str):
+    async def call(self, input: str, replied_to_message_content: str = ""):
+        prompt_args = {"input": input, "replied_to_message_content": replied_to_message_content}
+
         try:
-            response = self.llm_chain.invoke(prompt)
+            response = await self.llm_chain.ainvoke(prompt_args)
             logger.debug(f"AI response: {response}")
-            await ctx.respond(response.content)
+            return response.content
         except ConnectError as e:
             logger.error(f"Error caught. Using fallback LLM.")
             try:
                 self.__llm_swap()
                 self.llm_chain = self.prompt | self.current_llm
-                response = self.llm_chain.invoke(prompt)
+                response = await self.llm_chain.ainvoke(prompt_args)
                 logger.debug(f"AI response after fallback: {response}")
-                await ctx.respond(response.content)
+                return response.content
             except Exception as e:
                 logger.error(f"Unexpected error caught. Error message: {str(e)}")
-
+                return "AI Error"
+    
     def __setupGroqLLM(self, groq_llm_model: str):
         return ChatGroq(
             model=groq_llm_model,
