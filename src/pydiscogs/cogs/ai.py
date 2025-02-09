4a04e41dd3bda5629416dd17aed33b490d11ff06
bpafoshizle <bpafoshizle@gmail.com>
2025-02-09 16:13:22 -0600

    worked in gemini 2.0 flash. working on tool calling with duckduckgo search. doesn't seem to be calling tools atm, though. need to troubleshoot more

--- src/pydiscogs/cogs/ai.py
+++ src/pydiscogs/cogs/ai.py
@@ -1,12 +1,19 @@
 import logging
 import os
 from httpx import ConnectError
+from typing import List, Dict
 
 import discord
 from discord.ext import commands
-from langchain_ollama import ChatOllama
+
+from duckduckgo_search import DDGS
+
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
+from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.tools import tool
+
 
 logger = logging.getLogger(__name__)
 
@@ -15,12 +22,17 @@ class AI(commands.Cog):
         self,
         bot,
         ollama_endpoint: str = os.getenv("OLLAMA_ENDPOINT"),
+        google_api_key: str = os.getenv("GOOGLE_API_KEY"),
+        google_llm_model: str = os.getenv("GOOGLE_LLM_MODEL"),
         groq_api_key: str = os.getenv("GROQ_API_KEY"),
         groq_llm_model: str = os.getenv("GROQ_LLM_MODEL"),
         ai_system_prompt: str = os.getenv("AI_SYSTEM_PROMPT")
     ):
         self.ai_handler = AIHandler(
-            ollama_endpoint, groq_api_key, groq_llm_model, ai_system_prompt
+            ollama_endpoint, 
+            google_api_key, google_llm_model, 
+            groq_api_key, groq_llm_model, 
+            ai_system_prompt
         )
         self.bot = bot
     
@@ -51,14 +63,24 @@ class AIReplyModal(discord.ui.Modal):
 class AIHandler():
     def __init__(self,
         ollama_endpoint: str = os.getenv("OLLAMA_ENDPOINT"),
+        google_api_key: str = os.getenv("GOOGLE_API_KEY"),
+        google_llm_model: str = os.getenv("GOOGLE_LLM_MODEL"),
         groq_api_key: str = os.getenv("GROQ_API_KEY"),
         groq_llm_model: str = os.getenv("GROQ_LLM_MODEL"),
         ai_system_prompt: str = os.getenv("AI_SYSTEM_PROMPT")
     ):
-        if ollama_endpoint is None and groq_api_key is None:
+        if not any([ollama_endpoint, groq_api_key, google_api_key]):
             raise ValueError(
-                "Must specify either ollama_endpoint or groq_api_key"
+                "Must specify either ollama_endpoint, groq_api_key, or google_api_key"
             )
+        
+        self.ollama_endpoint = ollama_endpoint
+        self.groq_llm_model = groq_llm_model
+        self.google_llm_model = google_llm_model
+
+        self.ai_system_prompt = ai_system_prompt
+
+        self.tools = self.__get_tools()
         
         self.prompt = ChatPromptTemplate.from_messages(
             [
@@ -66,18 +88,12 @@ class AIHandler():
                     "system",
                     ai_system_prompt,
                 ),
-                ("human", "replied to message: {replied_to_message_content}"),
-                ("human", "{input}"),
+                ("human", "previous message being replied to: {replied_to_message_content}. New message: {input}"),
             ]
         )
 
-        self.ollama_llm = self.__setupOllamaLLM(ollama_endpoint)
-        self.groq_llm = self.__setupGroqLLM(groq_llm_model)
+        self.__setupLLMs()
         
-        if ollama_endpoint and groq_api_key:
-            self.current_llm = self.ollama_llm
-            self.fallback_llm = self.groq_llm
-
         self.llm_chain = self.prompt | self.current_llm
 
     async def call(self, input: str, replied_to_message_content: str = ""):
@@ -90,7 +106,7 @@ class AIHandler():
         except ConnectError as e:
             logger.error(f"Error caught. Using fallback LLM.")
             try:
-                self.__llm_swap()
+                self.__llm_cycle()
                 self.llm_chain = self.prompt | self.current_llm
                 response = await self.llm_chain.ainvoke(prompt_args)
                 logger.debug(f"AI response after fallback: {response}")
@@ -98,6 +114,23 @@ class AIHandler():
             except Exception as e:
                 logger.error(f"Unexpected error caught. Error message: {str(e)}")
                 return "AI Error"
+            
+    def __setupLLMs(self):
+        if self.ollama_endpoint:
+            self.ollama_llm = self.__setupOllamaLLM(self.ollama_endpoint)
+            self.ollama_llm.bind_tools(self.tools)
+        
+        if self.groq_llm_model:
+            self.groq_llm = self.__setupGroqLLM(self.groq_llm_model)
+            self.groq_llm.bind_tools(self.tools)
+
+        if self.google_llm_model:
+            self.google_llm = self.__setupGoogleLLM(self.google_llm_model)
+            self.google_llm.bind_tools(self.tools)
+
+        self.current_llm = self.ollama_llm
+        self.fallback_llms = [self.google_llm, self.groq_llm]
+
     
     def __setupGroqLLM(self, groq_llm_model: str):
         return ChatGroq(
@@ -114,5 +147,29 @@ class AIHandler():
             temperature=0,
         )
     
-    def __llm_swap(self):
-        self.current_llm, self.fallback_llm = self.fallback_llm, self.current_llm
+    def __setupGoogleLLM(self, google_llm_model: str):
+        return ChatGoogleGenerativeAI(
+            model=google_llm_model,
+            temperature=0.0,
+            max_retries=2,
+        )
+    
+    def __llm_cycle(self):
+        # Cycle through fallback_llms list, rotating the first element to the end.
+        self.current_llm = self.fallback_llms[0]
+        self.fallback_llms = self.fallback_llms[1:] + [self.fallback_llms[0]]
+
+    def __get_tools(self):
+        @tool
+        def web_search_text_ddg(query: str) -> List[Dict]:
+            """Search the web for text content using DuckDuckGo."""
+            results = DDGS.text(query, max_results=10)
+            logger.info(results)
+            return 
+        
+        @tool
+        def web_search_image_ddg(query: str) -> List[Dict]:
+            """Search the web for images using DuckDuckGo."""
+            return DDGS.images(query, max_results=10)
+        
+        return [web_search_text_ddg, web_search_image_ddg]
